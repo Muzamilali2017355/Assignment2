795b655780f590221ff4516332552cc5e513510b
Muzamilali2017355 <73329018+Muzamilali2017355@users.noreply.github.com>
2020-11-02 11:15:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Devops Assignment.
+          Devops Assignment 12345.
         </p>
         <a
           className="App-link"
